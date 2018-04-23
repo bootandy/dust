e6c777fb8bfe079b6a517abd4f8a80ff6611e251
andy.boot <bootandy@gmail.com>
2018-04-23 15:39:20 +0100

    Add support for -d depth flag
    Following a user request the option '-d N' allows a user to output N
    levels of sub directories.
    
    Fixed bug: so that trailing slashes are now removed.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ extern crate walkdir;
 
 use self::display::draw_it;
 use clap::{App, AppSettings, Arg};
-use utils::{find_big_ones, get_dir_tree};
+use std::io::{self, Write};
+use utils::{find_big_ones, get_dir_tree, sort};
 
 mod display;
 mod utils;
@@ -15,6 +16,12 @@ static DEFAULT_NUMBER_OF_LINES: &'static str = "15";
 fn main() {
     let options = App::new("Dust")
         .setting(AppSettings::TrailingVarArg)
+        .arg(
+            Arg::with_name("depth")
+                .short("d")
+                .help("Depth to show")
+                .takes_value(true),
+        )
         .arg(
             Arg::with_name("number_of_lines")
                 .short("n")
@@ -41,13 +48,46 @@ fn main() {
             Some(r) => r.collect(),
         }
     };
+
     let number_of_lines = value_t!(options.value_of("number_of_lines"), usize).unwrap();
+    let depth = {
+        if options.is_present("depth") {
+            match value_t!(options.value_of("depth"), u64) {
+                Ok(v) => Some(v + 1),
+                Err(_) => None,
+            }
+        } else {
+            None
+        }
+    };
+    if options.is_present("depth")
+        && options.value_of("number_of_lines").unwrap() != DEFAULT_NUMBER_OF_LINES
+    {
+        io::stderr()
+            .write(b"Use either -n for number of directories to show. Or -d for depth. Not both")
+            .expect("Error writing to stderr. Oh the irony!");
+        return;
+    }
+
     let use_apparent_size = options.is_present("display_apparent_size");
     let use_full_path = options.is_present("display_full_paths");
 
-    let (permissions, nodes) = get_dir_tree(&filenames, use_apparent_size);
-    let biggest_ones = find_big_ones(nodes, number_of_lines);
-    draw_it(permissions, !use_full_path, filenames, biggest_ones);
+    let (permissions, nodes, top_level_names) = get_dir_tree(&filenames, use_apparent_size);
+    let sorted_data = sort(nodes);
+    let biggest_ones = {
+        if depth.is_none() {
+            find_big_ones(sorted_data, number_of_lines)
+        } else {
+            sorted_data
+        }
+    };
+    draw_it(
+        permissions,
+        !use_full_path,
+        depth,
+        top_level_names,
+        biggest_ones,
+    );
 }
 
 #[cfg(test)]
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -9,20 +9,35 @@ use std::path::PathBuf;
 mod platform;
 use self::platform::*;
 
-pub fn get_dir_tree(filenames: &Vec<&str>, apparent_size: bool) -> (bool, HashMap<String, u64>) {
+pub fn get_dir_tree(
+    filenames: &Vec<&str>,
+    apparent_size: bool,
+) -> (bool, HashMap<String, u64>, Vec<String>) {
     let mut permissions = 0;
     let mut inodes: HashSet<(u64, u64)> = HashSet::new();
     let mut data: HashMap<String, u64> = HashMap::new();
+    let mut top_level_names = Vec::new();
+
     for b in filenames {
+        let top_level_name = strip_end_slashes(b);
         examine_dir(
-            &Path::new(b).to_path_buf(),
+            &Path::new(&top_level_name).to_path_buf(),
             apparent_size,
             &mut inodes,
             &mut data,
             &mut permissions,
         );
+        top_level_names.push(top_level_name);
     }
-    (permissions == 0, data)
+    (permissions == 0, data, top_level_names)
+}
+
+fn strip_end_slashes(s: &str) -> String {
+    let mut new_name = String::from(s);
+    while new_name.chars().last() == Some('/') && new_name.len() != 1 {
+        new_name.pop();
+    }
+    new_name
 }
 
 fn examine_dir(
@@ -66,11 +81,14 @@ fn examine_dir(
     }
 }
 
-pub fn find_big_ones<'a>(data: HashMap<String, u64>, max_to_show: usize) -> Vec<(String, u64)> {
+pub fn sort<'a>(data: HashMap<String, u64>) -> Vec<(String, u64)> {
     let mut new_l: Vec<(String, u64)> = data.iter().map(|(a, b)| (a.clone(), *b)).collect();
     new_l.sort_by(|a, b| b.1.cmp(&a.1));
+    new_l
+}
 
-    if new_l.len() > max_to_show {
+pub fn find_big_ones<'a>(new_l: Vec<(String, u64)>, max_to_show: usize) -> Vec<(String, u64)> {
+    if max_to_show > 0 && new_l.len() > max_to_show {
         new_l[0..max_to_show + 1].to_vec()
     } else {
         new_l
