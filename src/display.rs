48a8acb4e985a1b0f1accdd85fe158b0b4321d14
andy.boot <bootandy@gmail.com>
2020-02-10 21:45:56 +0000

    delicious sexyness

--- src/display.rs
+++ src/display.rs
@@ -3,13 +3,14 @@ extern crate ansi_term;
 use self::ansi_term::Colour::Fixed;
 use self::ansi_term::Style;
 use crate::utils::Node;
-use terminal_size::{Width, Height, terminal_size};
+use terminal_size::{terminal_size, Height, Width};
 
+use std::cmp::max;
 use std::iter::repeat;
 use std::path::Path;
-use std::cmp::max;
 
 static UNITS: [char; 4] = ['T', 'G', 'M', 'K'];
+static BLOCKS: [char; 4] = ['█', '▓', '▒', '░'];
 
 pub struct DisplayData {
     pub short_paths: bool,
@@ -97,44 +98,88 @@ pub fn draw_it(
         terminal_size: terminal_size(),
     };
 
-    let longest_str_length = root_node.children.iter().map(|c| 
-        find_longest_dir_name(c, &display_data.get_first_chars(), &display_data)
-    ).max();
+    let longest_str_length = root_node
+        .children
+        .iter()
+        .map(|c| find_longest_dir_name(c, &display_data.get_first_chars(), &display_data))
+        .max();
 
-    let longest_str = 
-        match longest_str_length {
-            Some(l) => l,
-            None => 40, //FIX ME
-        };
+    let longest_str = match longest_str_length {
+        Some(l) => l,
+        None => 40, //FIX ME
+    };
+
+    let ww = {
+        if let Some((Width(w), Height(h))) = display_data.terminal_size {
+            w
+        } else {
+            80
+        }
+    } - 16;
 
     for c in display_data.get_children_from_node(root_node) {
         let first_tree_chars = display_data.get_first_chars();
         let base_size = c.size;
-        display_node(c, true, first_tree_chars, &display_data, base_size, longest_str);
+
+        let max_bar_length = ww as usize - longest_str;
+        let bar_text = repeat(BLOCKS[0]).take(max_bar_length).collect::<String>();
+        display_node(
+            c,
+            true,
+            first_tree_chars,
+            &display_data,
+            base_size,
+            longest_str,
+            bar_text,
+        );
     }
 }
 
 fn find_longest_dir_name(node: &Node, indent: &str, display_data: &DisplayData) -> usize {
-    let mut longest = get_printable_name(node.name.clone(), display_data, indent).chars().count();
+    let mut longest = get_printable_name(node.name.clone(), display_data, indent)
+        .chars()
+        .count();
 
     for c in node.children.iter() {
         // each tree drawing is 3 chars
-        let full_indent :String = indent.to_string() + "   ";
-        longest = max(longest, find_longest_dir_name(c, &*full_indent, display_data));
+        let full_indent: String = indent.to_string() + "   ";
+        longest = max(
+            longest,
+            find_longest_dir_name(c, &*full_indent, display_data),
+        );
     }
     longest
 }
 
 // can we make 2 of these one for pre and post order traversal?
-fn display_node(node: Node, is_biggest: bool, indent: &str, display_data: &DisplayData, base_size: u64, longest_string_length: usize) {
+fn display_node(
+    node: Node,
+    is_biggest: bool,
+    indent: &str,
+    display_data: &DisplayData,
+    base_size: u64,
+    longest_string_length: usize,
+    parent_bar: String,
+) {
     let mut num_siblings = node.children.len() as u64;
     let max_sibling = num_siblings;
     let new_indent = clean_indentation_string(indent);
     let name = node.name.clone();
     let size = node.size;
+    let percent_size = size as f32 / base_size as f32;
 
+    let bar_text = generate_bar(parent_bar, display_data, percent_size, longest_string_length);
     if !display_data.is_reversed {
-        print_this_node(&name, size, is_biggest, display_data, indent, base_size, longest_string_length);
+        print_this_node(
+            &name,
+            size,
+            percent_size,
+            is_biggest,
+            display_data,
+            indent,
+            bar_text.as_ref(),
+            longest_string_length,
+        );
     }
 
     for c in display_data.get_children_from_node(node) {
@@ -143,11 +188,28 @@ fn display_node(node: Node, is_biggest: bool, indent: &str, display_data: &Displ
         // can we just do is first ? + handle reverse mode. consider equal values
         let is_biggest = display_data.is_biggest(num_siblings, max_sibling);
         let full_indent = new_indent.clone() + chars;
-        display_node(c, is_biggest, &*full_indent, display_data, base_size, longest_string_length);
+        display_node(
+            c,
+            is_biggest,
+            &*full_indent,
+            display_data,
+            base_size,
+            longest_string_length,
+            bar_text.clone()
+        );
     }
 
     if display_data.is_reversed {
-        print_this_node(&name, size, is_biggest, display_data, indent, base_size, longest_string_length);
+        print_this_node(
+            &name,
+            size,
+            percent_size,
+            is_biggest,
+            display_data,
+            indent,
+            bar_text.as_ref(),
+            longest_string_length,
+        );
     }
 }
 
@@ -171,17 +233,26 @@ fn clean_indentation_string(s: &str) -> String {
 fn print_this_node<P: AsRef<Path>>(
     name: P,
     size: u64,
+    percent_size: f32,
     is_biggest: bool,
     display_data: &DisplayData,
     indentation: &str,
-    base_size: u64,
-    longest_string_length: usize,
+    bar_text: &str,
+    longest_str_length: usize,
 ) {
     let pretty_size = format!("{:>5}", human_readable_number(size),);
-    let percent_size = size as f32 / base_size as f32;
     println!(
         "{}",
-        format_string(name, is_biggest, display_data, &*pretty_size, percent_size, indentation, longest_string_length)
+        format_string(
+            name,
+            is_biggest,
+            display_data,
+            &*pretty_size,
+            percent_size,
+            indentation,
+            bar_text,
+            longest_str_length
+        )
     )
 }
 
@@ -204,7 +275,50 @@ fn get_printable_name<P: AsRef<Path>>(
             dir_name
         }
     };
-    format!("{} {}", indentation , printable_name.display())
+    format!("{} {}", indentation, printable_name.display())
+}
+
+fn generate_bar(
+    parent_bar: String,
+    display_data: &DisplayData,
+    percent_size: f32,
+    longest_string_length: usize,
+) -> String {
+    let num_bars = (parent_bar.chars().count() as f32 * percent_size) as usize;
+    let mut num_not_my_bar = (parent_bar.chars().count() - num_bars) as i32;
+
+    // recall darkest seen so far
+    // while bright convert to darkest. if we have reached point then no conversion needed.
+    let mut new_bar = "".to_string();
+    let mut to_push = BLOCKS[3];
+
+    for c in parent_bar.chars() {
+        num_not_my_bar -= 1;
+        if num_not_my_bar <= 0 {
+            new_bar.push(BLOCKS[0]);
+        } 
+        else if c == BLOCKS[0] {
+            //push second darkest seen
+            new_bar.push(to_push);
+        }
+        else if c == BLOCKS[3] {
+            to_push = BLOCKS[2];
+            new_bar.push(c);
+        }
+        else if c == BLOCKS[2] {
+            to_push = BLOCKS[1];
+            new_bar.push(c);
+        }
+        else if c == BLOCKS[1] {
+            new_bar.push(c);
+        }
+    }
+
+    new_bar
+    // let c = BLOCKS[0]; //"▇"
+    // let markers = (repeat(" ").take(num_spaces).collect::<String>())
+    //     + (&(repeat(c).take(num_bars).collect::<String>()));
+    // markers
 }
 
 pub fn format_string<P: AsRef<Path>>(
@@ -214,39 +328,29 @@ pub fn format_string<P: AsRef<Path>>(
     size: &str,
     percent_size: f32,
     indentation: &str,
-    longest_string_length: usize,
+    bar_text: &str,
+    longest_str_length: usize,
 ) -> String {
-
     let percent_size_str = format!("{:.0}%", percent_size * 100.0);
 
     let dir_name = dir_name.as_ref();
     let tree_and_path = get_printable_name(dir_name, display_data, indentation);
-     
-    let ww = {
-        if let Some((Width(w), Height(h))) = display_data.terminal_size {
-            w
-        } else {
-            80
-        }
-    } - 16;
 
     let printable_chars = tree_and_path.chars().count();
-    let tree_and_path = tree_and_path + &(repeat(" ").take(longest_string_length - printable_chars).collect::<String>());
+    let tree_and_path = tree_and_path
+        + &(repeat(" ")
+            .take(longest_str_length - printable_chars)
+            .collect::<String>());
 
-    let max_bar_length = ww as usize - longest_string_length;
-    let num_bars = (max_bar_length as f32 * percent_size) as usize;
-    let num_spaces = max_bar_length - num_bars;
-    let markers = (repeat(" ").take(num_spaces).collect::<String>()) + &(repeat("▇").take(num_bars).collect::<String>());
-    
     format!(
-        "{} {} | {} │ {:>4}",
+        "{} {} │ {} │ {:>4}",
         if is_biggest && display_data.colors_on {
             Fixed(196).paint(size)
         } else {
             Style::new().paint(size)
         },
         tree_and_path,
-        markers,
+        bar_text,
         percent_size_str,
     )
 }
