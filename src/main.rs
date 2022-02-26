9f4a5daee6d6bed06206beba85bfcc1bc597f6b8
andy.boot <bootandy@gmail.com>
2022-02-26 10:51:30 +0000

    Update library: Clap
    Required several changes to main.rs as v3 had breaking changes.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate clap;
 extern crate rayon;
 extern crate regex;
@@ -8,10 +7,9 @@ use std::collections::HashSet;
 use std::process;
 
 use self::display::draw_it;
-use clap::Values;
-use clap::{App, AppSettings, Arg};
-use dir_walker::walk_it;
-use dir_walker::WalkData;
+use clap::{crate_version, Arg};
+use clap::{Command, Values};
+use dir_walker::{walk_it, WalkData};
 use filter::{get_all_file_types, get_biggest, get_by_depth};
 use regex::Regex;
 use std::cmp::max;
@@ -104,125 +102,125 @@ fn main() {
     let default_height = get_height_of_terminal();
     let def_num_str = default_height.to_string();
 
-    let options = App::new("Dust")
+    let options = Command::new("Dust")
         .about("Like du but more intuitive")
         .version(crate_version!())
-        .setting(AppSettings::TrailingVarArg)
+        .trailing_var_arg(true)
         .arg(
-            Arg::with_name("depth")
-                .short("d")
+            Arg::new("depth")
+                .short('d')
                 .long("depth")
                 .help("Depth to show")
                 .takes_value(true)
                 .conflicts_with("number_of_lines"),
         )
         .arg(
-            Arg::with_name("number_of_lines")
-                .short("n")
+            Arg::new("number_of_lines")
+                .short('n')
                 .long("number-of-lines")
                 .help("Number of lines of output to show. (Default is terminal_height - 10)")
                 .takes_value(true)
                 .default_value(def_num_str.as_ref()),
         )
         .arg(
-            Arg::with_name("display_full_paths")
-                .short("p")
+            Arg::new("display_full_paths")
+                .short('p')
                 .long("full-paths")
                 .help("Subdirectories will not have their path shortened"),
         )
         .arg(
-            Arg::with_name("ignore_directory")
-                .short("X")
+            Arg::new("ignore_directory")
+                .short('X')
                 .long("ignore-directory")
                 .takes_value(true)
                 .number_of_values(1)
-                .multiple(true)
+                .multiple_occurrences(true)
                 .help("Exclude any file or directory with this name"),
         )
         .arg(
-            Arg::with_name("limit_filesystem")
-                .short("x")
+            Arg::new("limit_filesystem")
+                .short('x')
                 .long("limit-filesystem")
                 .help("Only count the files and directories on the same filesystem as the supplied directory"),
         )
         .arg(
-            Arg::with_name("display_apparent_size")
-                .short("s")
+            Arg::new("display_apparent_size")
+                .short('s')
                 .long("apparent-size")
                 .help("Use file length instead of blocks"),
         )
         .arg(
-            Arg::with_name("reverse")
-                .short("r")
+            Arg::new("reverse")
+                .short('r')
                 .long("reverse")
                 .help("Print tree upside down (biggest highest)"),
         )
         .arg(
-            Arg::with_name("no_colors")
-                .short("c")
+            Arg::new("no_colors")
+                .short('c')
                 .long("no-colors")
                 .help("No colors will be printed (Useful for commands like: watch)"),
         )
         .arg(
-            Arg::with_name("no_bars")
-                .short("b")
+            Arg::new("no_bars")
+                .short('b')
                 .long("no-percent-bars")
                 .help("No percent bars or percentages will be displayed"),
         )
         .arg(
-            Arg::with_name("by_filecount")
-                .short("f")
+            Arg::new("by_filecount")
+                .short('f')
                 .long("filecount")
                 .help("Directory 'size' is number of child files/dirs not disk size"),
         )
         .arg(
-            Arg::with_name("ignore_hidden")
-                .short("i") // Do not use 'h' this is used by 'help'
+            Arg::new("ignore_hidden")
+                .short('i') // Do not use 'h' this is used by 'help'
                 .long("ignore_hidden")
                 .help("Do not display hidden files"),
         )
         .arg(
-            Arg::with_name("invert_filter")
-                .short("v")
+            Arg::new("invert_filter")
+                .short('v')
                 .long("invert-filter")
                 .takes_value(true)
                 .number_of_values(1)
-                .multiple(true)
+                .multiple_occurrences(true)
                 .conflicts_with("filter")
                 .conflicts_with("types")
                 .conflicts_with("depth")
                 .help("Exclude files matching this regex. To ignore png files type: -v \"\\.png$\" "),
         )
         .arg(
-            Arg::with_name("filter")
-                .short("e")
+            Arg::new("filter")
+                .short('e')
                 .long("filter")
                 .takes_value(true)
                 .number_of_values(1)
-                .multiple(true)
+                .multiple_occurrences(true)
                 .conflicts_with("types")
                 .conflicts_with("depth")
                 .help("Only include files matching this regex. For png files type: -e \"\\.png$\" "),
         )
         .arg(
-            Arg::with_name("types")
-                .short("t")
+            Arg::new("types")
+                .short('t')
                 .long("file_types")
                 .conflicts_with("depth")
                 .help("show only these file types"),
         )
         .arg(
-            Arg::with_name("width")
-                .short("w")
+            Arg::new("width")
+                .short('w')
                 .long("terminal_width")
                 .takes_value(true)
                 .number_of_values(1)
                 .help("Specify width of output overriding the auto detection of terminal width"),
         )
-        .arg(Arg::with_name("inputs").multiple(true).default_value("."))
+        .arg(Arg::new("inputs").multiple_occurrences(true).default_value("."))
         .arg(
-            Arg::with_name("iso")
-                .short("H")
+            Arg::new("iso")
+                .short('H')
                 .long("si")
                 .help("print sizes in powers of 1000 (e.g., 1.1G)")
         )
@@ -238,7 +236,7 @@ fn main() {
     let filter_regexs = get_regex_value(options.values_of("filter"));
     let invert_filter_regexs = get_regex_value(options.values_of("invert_filter"));
 
-    let number_of_lines = match value_t!(options.value_of("number_of_lines"), usize) {
+    let number_of_lines = match options.value_of_t("number_of_lines") {
         Ok(v) => v,
         Err(_) => {
             eprintln!("Ignoring bad value for number_of_lines");
@@ -246,7 +244,7 @@ fn main() {
         }
     };
 
-    let terminal_width = match value_t!(options.value_of("width"), usize) {
+    let terminal_width = match options.value_of_t("width") {
         Ok(v) => v,
         Err(_) => get_width_of_terminal(),
     };
