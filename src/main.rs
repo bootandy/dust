312651839596f128aeb8d21df2228224de7b0970
andy.boot <bootandy@gmail.com>
2023-01-04 19:45:33 +0000

    fix: update use of sysinfo.system
    System is now much quicker to start but requires an explicit call
    to refresh memory else it deafults to 0 (oops)

--- src/main.rs
+++ src/main.rs
@@ -208,8 +208,8 @@ fn main() {
 
 fn init_rayon() -> Result<(), ThreadPoolBuildError> {
     let large_stack = usize::pow(1024, 3);
-    // Warning: Creating System is slow, takes ~ 100ms
-    let s = System::new();
+    let mut s = System::new();
+    s.refresh_memory();
     let available = s.available_memory();
 
     if available > large_stack.try_into().unwrap() {
