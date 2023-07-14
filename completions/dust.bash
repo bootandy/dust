0038cb24b4adbb85321e3c36fb7079fc898de44e
wickles <4229542+wickles@users.noreply.github.com>
2023-07-14 10:32:21 -0700

    Fix filename completions for zsh and bash (#331)

--- completions/dust.bash
+++ completions/dust.bash
@@ -20,8 +20,11 @@ _dust() {
     case "${cmd}" in
         dust)
             opts="-h -V -d -n -p -X -L -x -s -r -c -b -z -R -f -i -v -e -t -w -H -P -D -F --help --version --depth --number-of-lines --full-paths --ignore-directory --dereference-links --limit-filesystem --apparent-size --reverse --no-colors --no-percent-bars --min-size --screen-reader --skip-total --filecount --ignore_hidden --invert-filter --filter --file_types --terminal_width --si --no-progress --only-dir --only-file <inputs>..."
-            if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+            if [[ ${cur} == -* ]] ; then
                 COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            elif [[ ${cur} == * ]] ; then
+                _filedir
                 return 0
             fi
             case "${prev}" in
