02dd6b0dbbeb523fa0ec0744b8f33a5f93d5d27f
andy.boot <bootandy@gmail.com>
2023-11-21 23:13:07 +0000

    dependency: upgrade to clap 4

--- completions/dust.fish
+++ completions/dust.fish
@@ -5,8 +5,6 @@ complete -c dust -s z -l min-size -d 'Minimum size file to include in output' -r
 complete -c dust -s v -l invert-filter -d 'Exclude filepaths matching this regex. To ignore png files type: -v "\\.png$" ' -r
 complete -c dust -s e -l filter -d 'Only include filepaths matching this regex. For png files type: -e "\\.png$" ' -r
 complete -c dust -s w -l terminal_width -d 'Specify width of output overriding the auto detection of terminal width' -r
-complete -c dust -s h -l help -d 'Print help information'
-complete -c dust -s V -l version -d 'Print version information'
 complete -c dust -s p -l full-paths -d 'Subdirectories will not have their path shortened'
 complete -c dust -s L -l dereference-links -d 'dereference sym links - Treat sym links as directories and go into them'
 complete -c dust -s x -l limit-filesystem -d 'Only count the files and directories on the same filesystem as the supplied directory'
@@ -24,3 +22,5 @@ complete -c dust -s H -l si -d 'print sizes in powers of 1000 (e.g., 1.1G)'
 complete -c dust -s P -l no-progress -d 'Disable the progress indication.'
 complete -c dust -s D -l only-dir -d 'Only directories will be displayed.'
 complete -c dust -s F -l only-file -d 'Only files will be displayed. (Finds your largest files)'
+complete -c dust -s h -l help -d 'Print help'
+complete -c dust -s V -l version -d 'Print version'
--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,9 @@
-use clap::{Arg, Command};
+use clap::{value_parser, Arg, Command};
 
 // For single thread mode set this variable on your command line:
 // export RAYON_NUM_THREADS=1
 
-pub fn build_cli() -> Command<'static> {
+pub fn build_cli() -> Command {
     Command::new("Dust")
         .about("Like du but more intuitive")
         .version(env!("CARGO_PKG_VERSION"))
@@ -12,111 +12,120 @@ pub fn build_cli() -> Command<'static> {
             Arg::new("depth")
                 .short('d')
                 .long("depth")
+                .value_parser(value_parser!(usize))
                 .help("Depth to show")
-                .takes_value(true)
+                .num_args(1)
         )
         .arg(
             Arg::new("number_of_lines")
                 .short('n')
                 .long("number-of-lines")
+                .value_parser(value_parser!(usize))
                 .help("Number of lines of output to show. (Default is terminal_height - 10)")
-                .takes_value(true)
+                .num_args(1)
         )
         .arg(
             Arg::new("display_full_paths")
                 .short('p')
                 .long("full-paths")
+                .action(clap::ArgAction::SetTrue)
                 .help("Subdirectories will not have their path shortened"),
         )
         .arg(
             Arg::new("ignore_directory")
                 .short('X')
                 .long("ignore-directory")
-                .takes_value(true)
-                .number_of_values(1)
-                .multiple_occurrences(true)
+                .action(clap::ArgAction::Append)
                 .help("Exclude any file or directory with this name"),
         )
          .arg(
             Arg::new("dereference_links")
                 .short('L')
                 .long("dereference-links")
+                .action(clap::ArgAction::SetTrue)
                 .help("dereference sym links - Treat sym links as directories and go into them"),
         )
         .arg(
             Arg::new("limit_filesystem")
                 .short('x')
                 .long("limit-filesystem")
+                .action(clap::ArgAction::SetTrue)
                 .help("Only count the files and directories on the same filesystem as the supplied directory"),
         )
         .arg(
             Arg::new("display_apparent_size")
                 .short('s')
                 .long("apparent-size")
+                .action(clap::ArgAction::SetTrue)
                 .help("Use file length instead of blocks"),
         )
         .arg(
             Arg::new("reverse")
                 .short('r')
                 .long("reverse")
+                .action(clap::ArgAction::SetTrue)
                 .help("Print tree upside down (biggest highest)"),
         )
         .arg(
             Arg::new("no_colors")
                 .short('c')
                 .long("no-colors")
+                .action(clap::ArgAction::SetTrue)
                 .help("No colors will be printed (Useful for commands like: watch)"),
         )
         .arg(
             Arg::new("no_bars")
                 .short('b')
                 .long("no-percent-bars")
+                .action(clap::ArgAction::SetTrue)
                 .help("No percent bars or percentages will be displayed"),
         )
         .arg(
             Arg::new("bars_on_right")
                 .short('B')
                 .long("bars-on-right")
+                .action(clap::ArgAction::SetTrue)
                 .help("percent bars moved to right side of screen"),
         )
         .arg(
             Arg::new("min_size")
                 .short('z')
                 .long("min-size")
-                .takes_value(true)
-                .number_of_values(1)
+                .num_args(1)
                 .help("Minimum size file to include in output"),
         )
         .arg(
             Arg::new("screen_reader")
                 .short('R')
                 .long("screen-reader")
+                .action(clap::ArgAction::SetTrue)
                 .help("For screen readers. Removes bars. Adds new column: depth level (May want to use -p too for full path)"),
         )
         .arg(
             Arg::new("skip_total")
                 .long("skip-total")
+                .action(clap::ArgAction::SetTrue)
                 .help("No total row will be displayed"),
         )
         .arg(
             Arg::new("by_filecount")
                 .short('f')
                 .long("filecount")
+                .action(clap::ArgAction::SetTrue)
                 .help("Directory 'size' is number of child files instead of disk size"),
         )
         .arg(
             Arg::new("ignore_hidden")
                 .short('i') // Do not use 'h' this is used by 'help'
                 .long("ignore_hidden")
+                .action(clap::ArgAction::SetTrue)
                 .help("Do not display hidden files"),
         )
         .arg(
             Arg::new("invert_filter")
                 .short('v')
                 .long("invert-filter")
-                .takes_value(true)
-                .number_of_values(1)
-                .multiple_occurrences(true)
+                .action(clap::ArgAction::Append)
                 .conflicts_with("filter")
                 .conflicts_with("types")
                 .help("Exclude filepaths matching this regex. To ignore png files type: -v \"\\.png$\" "),
@@ -125,9 +134,7 @@ pub fn build_cli() -> Command<'static> {
             Arg::new("filter")
                 .short('e')
                 .long("filter")
-                .takes_value(true)
-                .number_of_values(1)
-                .multiple_occurrences(true)
+                .action(clap::ArgAction::Append)
                 .conflicts_with("types")
                 .help("Only include filepaths matching this regex. For png files type: -e \"\\.png$\" "),
         )
@@ -137,26 +144,29 @@ pub fn build_cli() -> Command<'static> {
                 .long("file_types")
                 .conflicts_with("depth")
                 .conflicts_with("only_dir")
+                .action(clap::ArgAction::SetTrue)
                 .help("show only these file types"),
         )
         .arg(
             Arg::new("width")
                 .short('w')
                 .long("terminal_width")
-                .takes_value(true)
-                .number_of_values(1)
+                .num_args(1)
+                .value_parser(value_parser!(usize))
                 .help("Specify width of output overriding the auto detection of terminal width"),
         )
         .arg(
             Arg::new("iso")
                 .short('H')
                 .long("si")
+                .action(clap::ArgAction::SetTrue)
                 .help("print sizes in powers of 1000 (e.g., 1.1G)")
         )
         .arg(
             Arg::new("disable_progress")
                 .short('P')
                 .long("no-progress")
+                .action(clap::ArgAction::SetTrue)
                 .help("Disable the progress indication."),
         )
         .arg(
@@ -165,6 +175,7 @@ pub fn build_cli() -> Command<'static> {
                 .long("only-dir")
                 .conflicts_with("only_file")
                 .conflicts_with("types")
+                .action(clap::ArgAction::SetTrue)
                 .help("Only directories will be displayed."),
         )
         .arg(
@@ -172,7 +183,9 @@ pub fn build_cli() -> Command<'static> {
                 .short('F')
                 .long("only-file")
                 .conflicts_with("only_dir")
+                .action(clap::ArgAction::SetTrue)
                 .help("Only files will be displayed. (Finds your largest files)"),
         )
-        .arg(Arg::new("inputs").multiple_occurrences(true))
+        .arg(Arg::new("params").num_args(1..)
+            .value_parser(value_parser!(String)))
 }
