84fa0ea9a4bb383c2db7f3c7f3084d4271fc54bb
andy.boot <bootandy@gmail.com>
2025-07-02 18:20:27 +0100

    fix: dir_walker interrupted error
    Ignore and continue if we get many interrupted errors instead of
    panicing on 3.
    
    This is from user feedback who reported occasional panics from too many
    interrupted errors
    
    https://github.com/bootandy/dust/issues/495#issuecomment-3026673312

--- src/dir_walker.rs
+++ src/dir_walker.rs
@@ -305,11 +305,7 @@ fn handle_error_and_retry(failed: &Error, dir: &Path, walk_data: &WalkData) -> b
         }
         std::io::ErrorKind::Interrupted => {
             editable_error.interrupted_error += 1;
-            if editable_error.interrupted_error > 3 {
-                panic!("Multiple Interrupted Errors occurred while scanning filesystem. Aborting");
-            } else {
-                return true;
-            }
+            return true;
         }
         _ => {
             editable_error.unknown_error.insert(failed.to_string());
